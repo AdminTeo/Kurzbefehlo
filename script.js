413c9dc683f1f6e75764f837919df5b1772016aa
Granatapfli <160935926+AdminTeo@users.noreply.github.com>
2024-10-12 02:47:30 +0200

    Attract Hover Button
    added a attract function to .hover-button

--- script.js
+++ script.js
@@ -26,3 +26,76 @@ function copyToClipboard(element, text) {
         }
     }, 2000);
 }
+
+
+class HoverButton {
+    constructor(el) {
+        this.el = el;
+        this.hover = false;
+        this.calculatePosition();
+        this.attachEventsListener();
+    }
+
+    attachEventsListener() {
+        window.addEventListener('mousemove', e => this.onMouseMove(e));
+        window.addEventListener('resize', e => this.calculatePosition(e));
+    }
+
+    calculatePosition() {
+        const box = this.el.getBoundingClientRect();
+        this.x = box.left + (box.width * 0.5);
+        this.y = box.top + (box.height * 0.5);
+        this.width = box.width;
+        this.height = box.height;
+    }
+
+    onMouseMove(e) {
+        let hover = false;
+        let hoverArea = 0.5; // Ein kleinerer Bereich für das Hover-Attract
+        let x = e.clientX - this.x;
+        let y = e.clientY - this.y;
+        let distance = Math.sqrt(x * x + y * y);
+
+        if (distance < (this.width * hoverArea)) {
+            hover = true;
+            if (!this.hover) {
+                this.hover = true;
+            }
+            this.onHover(e.clientX, e.clientY);
+        }
+
+        if (!hover && this.hover) {
+            this.onLeave();
+            this.hover = false;
+        }
+    }
+
+    onHover(x, y) {
+        const offset = 30;
+        gsap.to(this.el, {
+            x: (x - this.x) * 0.4, // Reduziere die Anziehungskraft
+            y: (y - this.y) * 0.4,
+            scale: 1.1,
+            ease: 'power2.out',
+            duration: 0.4
+        });
+        this.el.style.zIndex = 10;
+    }
+
+    onLeave() {
+        gsap.to(this.el, {
+            x: 0,
+            y: 0,
+            scale: 1,
+            ease: 'elastic.out(1.2, 0.4)',
+            duration: 0.7
+        });
+        this.el.style.zIndex = 1;
+    }
+}
+
+// Hole alle Hover-Buttons in der Dropdown-Klasse
+const hoverButtons = document.querySelectorAll('.dropdown .hover-button');
+hoverButtons.forEach(button => {
+    new HoverButton(button);
+});
